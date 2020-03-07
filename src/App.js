54f3e91a3f6d896a5e1e158784412e3c08b02af1
Williamcsa04 <williamcaetano14@gmail.com>
2020-03-07 15:26:11 -0300

    :construction: Added logic to get data from championships

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
+import { get } from "./services";
 
 export default function App() {
-  return <div>Hello Mocks</div>;
+  const [cupData, setCupData] = useState([])
+  const [leagueData, setLeagueData] = useState([])
+  useEffect(() => {
+    get('cup').then(response => setCupData(response))
+    get('league').then(response => setLeagueData(response))
+  }, [])
+  return (
+    <>
+      <div>Hello Mocks</div>
+      <div>Cup: {JSON.stringify(cupData)}</div>
+      <div>League: {JSON.stringify(leagueData)}</div>
+    </>
+  );
 }
