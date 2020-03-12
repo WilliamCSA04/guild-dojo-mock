2d001d8eedcc9a044bf3cda537265c88b334fe75
Williamcsa04 <williamcaetano14@gmail.com>
2020-03-12 00:16:07 -0300

    :white_check_mark: Created tests to verify return property

--- src/services/Football.js
+++ src/services/Football.js
@@ -1,14 +1,20 @@
-import { get as CupGetter } from './BrazilianCup.js'
-import { get as LeagueGetter } from './BrazilianLeague.js'
+import { get as CupGetter } from "./BrazilianCup.js";
+import { get as LeagueGetter } from "./BrazilianLeague.js";
 
 function get(championship) {
   const lowerCased = championship.toLowerCase();
-  if(lowerCased === 'league') {
-    return LeagueGetter();
+  if (lowerCased === "league") {
+    return LeagueGetter().then(response => {
+      const [teams, matches] = response;
+      return { teams, matches };
+    });
   }
-  if(lowerCased === 'cup') {
-    return CupGetter();
+  if (lowerCased === "cup") {
+    return CupGetter().then(response => {
+      const [teams, matches] = response;
+      return { teams, matches };
+    });
   }
 }
 
-export { get }
+export { get };
--- src/services/Football.test.js
+++ src/services/Football.test.js
@@ -12,14 +12,28 @@ describe("get by championship", () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
-  test("get from Brazilian League must be called", () => {
+  test("get('league') must call LeagueGetterSpy only", () => {
     return get("league").then(response => {
       expect(LeagueGetterSpy).toHaveBeenCalled();
+      expect(CupGetterSpy).not.toHaveBeenCalled();
     });
   });
-  test("get from Brazilian League must be called", () => {
+  test("get('cup') must call CupGetterSpy only", () => {
     return get("cup").then(response => {
       expect(CupGetterSpy).toHaveBeenCalled();
+      expect(LeagueGetterSpy).not.toHaveBeenCalled();
+    });
+  });
+  test("get('league') must return an object with matches and games", () => {
+    return get("league").then(response => {
+      expect(response).toHaveProperty("teams");
+      expect(response).toHaveProperty("matches");
+    });
+  });
+  test("get('cup') must return an object with matches and games", () => {
+    return get("cup").then(response => {
+      expect(response).toHaveProperty("teams");
+      expect(response).toHaveProperty("matches");
     });
   });
 });
