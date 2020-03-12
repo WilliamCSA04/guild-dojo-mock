b3c35a17f83d01d58893195b9ba18ea1e134fbdb
Williamcsa04 <williamcaetano14@gmail.com>
2020-03-11 23:51:47 -0300

    :white_check_mark: Created first test

--- src/services/Football.test.js
+++ src/services/Football.test.js
@@ -1,8 +1,12 @@
 import { get } from "./Football";
+import * as BrazilianLeague from "./BrazilianLeague";
+
 describe("get by championship", () => {
-  test("get", () => {
+  test("get from Brazilian League must be called", () => {
     const stub = jest.fn(get);
-    stub();
-    expect(stub).toHaveBeenCalled();
+    const LeagueGetterStub = jest.spyOn(BrazilianLeague, "get");
+    return stub("league").then(response => {
+      expect(LeagueGetterStub).toHaveBeenCalled();
+    });
   });
 });
