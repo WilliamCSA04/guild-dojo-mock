a3cbc01182f5a152a0e361f2fe05a66508c96cf3
Williamcsa04 <williamcaetano14@gmail.com>
2020-03-12 00:02:41 -0300

    :art: Added setup and clean up for tests

--- src/services/Football.test.js
+++ src/services/Football.test.js
@@ -3,14 +3,21 @@ import * as BrazilianLeague from "./BrazilianLeague";
 import * as BrazilianCup from "./BrazilianCup";
 
 describe("get by championship", () => {
+  let LeagueGetterSpy = null;
+  let CupGetterSpy = null;
+  beforeEach(() => {
+    LeagueGetterSpy = jest.spyOn(BrazilianLeague, "get");
+    CupGetterSpy = jest.spyOn(BrazilianCup, "get");
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   test("get from Brazilian League must be called", () => {
-    const LeagueGetterSpy = jest.spyOn(BrazilianLeague, "get");
     return get("league").then(response => {
       expect(LeagueGetterSpy).toHaveBeenCalled();
     });
   });
   test("get from Brazilian League must be called", () => {
-    const CupGetterSpy = jest.spyOn(BrazilianCup, "get");
     return get("cup").then(response => {
       expect(CupGetterSpy).toHaveBeenCalled();
     });
