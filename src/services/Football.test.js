4717d8cd3c6c5faa75b979e463c4060443ae9536
Williamcsa04 <williamcaetano14@gmail.com>
2020-03-19 08:42:42 -0300

    :fire: Removed code for dojo

--- src/services/Football.test.js
+++ src/services/Football.test.js
@@ -18,87 +18,33 @@ describe("get by championship", () => {
       expect(CupGetterSpy).not.toHaveBeenCalled();
     });
   });
-  test("get('cup') must call CupGetterSpy only", () => {
-    return get("cup").then(response => {
-      expect(CupGetterSpy).toHaveBeenCalled();
-      expect(LeagueGetterSpy).not.toHaveBeenCalled();
-    });
-  });
-  test("get('league') must return an object with matches and teams", () => {
-    return get("league").then(response => {
-      expect(response).toHaveProperty("teams");
-      expect(response).toHaveProperty("matches");
-    });
-  });
-  test("get('cup') must return an object with matches and teams", () => {
-    return get("cup").then(response => {
-      expect(response).toHaveProperty("teams");
-      expect(response).toHaveProperty("matches");
-    });
-  });
-  test("get('league') returned value must have array on each key", () => {
-    return get("league").then(response => {
-      expect(Array.isArray(response.teams)).toBeTruthy();
-      expect(Array.isArray(response.matches)).toBeTruthy();
-    });
-  });
-  test("get('cup') returned value must have array on each key", () => {
-    return get("cup").then(response => {
-      expect(Array.isArray(response.teams)).toBeTruthy();
-      expect(Array.isArray(response.matches)).toBeTruthy();
-    });
-  });
-  test("get('league') must return matches and teams as empty arrays if LeagueGetterSpy returns two empty arrays", () => {
-    LeagueGetterSpy.mockResolvedValue([[], []]);
-    return get("league").then(response => {
-      expect(response.teams.length).toBe(0);
-      expect(response.matches.length).toBe(0);
-    });
-  });
-  test("get('cup') returned value must have array on each key", () => {
-    CupGetterSpy.mockResolvedValue([[], []]);
-    return get("cup").then(response => {
-      expect(response.teams.length).toBe(0);
-      expect(response.matches.length).toBe(0);
-    });
-  });
-  test("get('league') every team must have an id that is an number", () => {
-    return get("league").then(response => {
-      response.teams.forEach(team => {
-        expect(team).toHaveProperty("id");
-        expect(Number.isInteger(team.id)).toBeTruthy();
-      });
-    });
-  });
-  test("get('cup') every team must have an id that is an number", () => {
-    return get("cup").then(response => {
-      response.teams.forEach(team => {
-        expect(team).toHaveProperty("id");
-        expect(Number.isInteger(team.id)).toBeTruthy();
-      });
-    });
-  });
+  test("get('cup') must call CupGetterSpy only", () => {});
+  test("get('league') must return an object with matches and teams", () => {});
+  test("get('cup') must return an object with matches and teams", () => {});
+  test("get('league') returned value must have array on each key", () => {});
+  test("get('cup') returned value must have array on each key", () => {});
+  test("get('league') must return matches and teams as empty arrays if LeagueGetterSpy returns two empty arrays", () => {});
+  test("get('cup') matches and values should return empty arrays if CupGetterSpy returns two empty arrays", () => {});
+  test("get('league') every team must have an id that is an number", () => {});
+  test("get('cup') every team must have an id that is an number", () => {});
   test("get('') should throw an error", () => {
-    expect(() => get("")).toThrow(Error);
+    expect(() => get(""));
   });
-  test("get('league') should call LeagueGetterSpy without args", () => {
-    return get("league").then(() => {
-      expect(LeagueGetterSpy).toHaveBeenCalledWith();
-    });
-  });
-  test("get('cup') should call CupGetterSpy without args", () => {
-    return get("cup").then(() => {
-      expect(CupGetterSpy).toHaveBeenCalledWith();
-    });
-  });
-  test("get('league') should call LeagueGetterSpy only once", () => {
-    return get("league").then(() => {
-      expect(LeagueGetterSpy).toHaveBeenCalledTimes(1);
-    });
-  });
-  test("get('cup') should call CupGetterSpy only once", () => {
-    return get("cup").then(() => {
-      expect(CupGetterSpy).toHaveBeenCalledTimes(1);
-    });
-  });
+  test("get('league') should call LeagueGetterSpy without args", () => {});
+  test("get('cup') should call CupGetterSpy without args", () => {});
+  test("get('league') should call LeagueGetterSpy only once", () => {});
+  test("get('cup') should call CupGetterSpy only once", () => {});
 });
+
+/*
+https://jestjs.io/docs/en/mock-function-api
+mockResolvedValue(responseValue);
+
+https://jestjs.io/docs/en/expect
+toHaveBeenCalledTimes(numberOfTimes);
+toHaveBeenCalledWith(params);
+toHaveProperty(propertyName);
+toBe(value);
+toThrow(TypeError);
+toBeTruthy();
+*/
